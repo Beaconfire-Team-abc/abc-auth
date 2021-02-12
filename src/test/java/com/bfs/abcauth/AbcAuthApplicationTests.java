2a7c6e56a23d5b5e9bdbb385b90262baeb44a188
KaiqingW <kkwalle0730@gmail.com>
2021-02-12 00:50:35 -0500

    project init

---
+++ src/test/java/com/bfs/abcauth/AbcAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfs.abcauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
